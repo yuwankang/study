e2920eac24b68f679644e15d0657b49f305437d4
kyw <kyw4330@gmail.com>
2023-12-17 12:26:25 +0900

    sizeof 및 형변환

---
+++ C/sizeofex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("%d %d %d %d\n", sizeof(int), sizeof(char), sizeof(float), sizeof(double));
+
+    int a; char b; float c; double d;
+    printf("%d %d %d %d\n", sizeof(a), sizeof(b), sizeof(c), sizeof(d));
+    
+}
+
+// sizeof(x): x의 크기를 알려줌
