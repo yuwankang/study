cbbeddcabd69c02494d575ab4f86d3a8ea05b593
yuwankang <kangyuwan@naver.com>
2023-12-28 18:01:09 +0900

    2차원 배열 과 파스칼의 삼각형

---
+++ C/Array2ex2.c
@@ -0,0 +1,19 @@
+//파스칼의 삼각형
+#include <stdio.h>
+
+int main(){
+    int p[10][10];
+
+    for(int i =0; i < 10; i++){
+        for(int j = 0; j <= i; j++){
+            if(j == 0 || j ==i){
+                p[i][j] = 1;
+            }
+            else{
+                p[i][j] = p[i-1][j-1] + p[i-1][j];
+            }
+            printf("%d ", p[i][j]);
+        }
+        printf("\n");
+    }
+}
