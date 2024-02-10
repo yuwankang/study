b830b492b7f0d5eb2263a8279fb727c3ae0ccb4f
yuwankang <kangyuwan@naver.com>
2024-02-10 15:40:31 +0900

    pointer 공부

--- C/pointer.c/pointer1.c
+++ C/pointer.c/pointer1.c
@@ -1,1 +1,16 @@
-#include
+#include <stdio.h>
+//포인터 = 변수의 주소를 저장하는 변수
+int main(){
+    int a = 20;
+
+    int *ptr_a;
+
+    ptr_a = &a; // &a로 값을 ptr_a에 저장 &는 주솟값
+
+    printf("%d\n", ptr_a);    
+
+    printf("a의 값: %d\n", a);
+    printf("a의 주솟값: %d\n", &a);
+    printf("ptr_a에 저장된 값: %d\n", ptr_a);
+    printf("ptr_a가 가리키는 변수의 값: %d\n", *ptr_a);
+}
---
+++ C/pointer.c/pointer4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+
+    int *ptr;
+    ptr = &a;
+
+    int **ptr_ptr;
+    ptr_ptr = &ptr;
+
+    printf("a = %d\n", a);
+    printf("&a = %d\n", &a);
+
+    printf("ptr = %d\n", ptr);
+    printf("&ptr %d\n", &ptr);
+
+    printf("**ptr_ptr = %d\n", **ptr_ptr);
+}
